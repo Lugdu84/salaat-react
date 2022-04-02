462e76c426593ac6925ebdf84e3ca5a6ece36fbe
David Grammatico <david.lugdunum@gmail.com>
2022-04-02 09:49:04 +0200

    with search bar

--- src/Components/Card/Salaat.js
+++ src/Components/Card/Salaat.js
@@ -1,26 +1,51 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import RowSalaat from './RowSalaat'
 import "./Salaat.scss"
 
 export default function Salaat() {
   const [salaats, setSalaats] = useState([])
+  const cityRef = useRef()
+
+  const searchSalaat = (city = "Paris") => {
+    fetch(
+      `https://api.aladhan.com/v1/timingsByCity?city=${city}&country=France&method=12`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setSalaats(data.data.timings)
+      });
+  }
 
   useEffect(() => {
 
-    fetch("https://api.aladhan.com/v1/timingsByCity?city=Paris&country=France&method=2")
-    .then(response => response.json())
-    .then(data => setSalaats(data.data.timings))
+    searchSalaat()
 
-    console.log(salaats);
-
-    return () => {
-
-    }
   },[])
+
+  const handleClick = () => {
+    const cityToSearch = `${cityRef.current.value
+      .charAt(0)
+      .toUpperCase()}${cityRef.current.value.substring(1).toLowerCase()}`;
+    searchSalaat(cityToSearch)
+    window.localStorage.setItem("citySalaat", cityToSearch);
+    cityRef.current.value=""
+  }
 
   return (
     <div className="container">
       <h1>Horaires des prières</h1>
+      <h2>{window.localStorage.getItem("citySalaat") ?? "Paris"}</h2>
+      <form>
+        <label
+          htmlFor=""
+          type="text"
+          text="Chechez les horaires de prières pour : "
+        >
+          Cherchez une ville :
+        </label>
+        <input type="text" name="city" ref={cityRef} />
+        <input className='button' type="button" value={"Validez"} onClick={handleClick} />
+      </form>
       <div className="card-salaat">
         <RowSalaat name={"Fajr"} times={salaats?.Fajr} />
         <RowSalaat name={"Chourouq"} times={salaats?.Sunrise} />
